a4a48f7ec479d9b7b926bfadafc692587361002f
Sergey <45532331+RABBITPYL9@users.noreply.github.com>
2022-02-19 18:11:16 +0300

    Create args.py

---
+++ args.py
@@ -0,0 +1,19 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser(description='Change host for run tests')
+parser.add_argument('testhost', type=str, help='host for run test')
+args = parser.parse_args()
+
+
+
+
+
+
+
+if args.testhost == "dev":
+    a = requests.get("https://ya.ru")
+    print(a.content)
+elif args.testhost == "prod":
+    b = requests.get("https://google.ru")
+    print(b.content)
