7990daebbebea846766b231f56648a788c81a98e
Sergey <45532331+RABBITPYL9@users.noreply.github.com>
2022-02-19 18:17:00 +0300

    Update args.py

--- args.py
+++ args.py
@@ -5,12 +5,6 @@ parser = argparse.ArgumentParser(description='Change host for run tests')
 parser.add_argument('testhost', type=str, help='host for run test')
 args = parser.parse_args()
 
-
-
-
-
-
-
 if args.testhost == "dev":
     a = requests.get("https://ya.ru")
     print(a.content)
